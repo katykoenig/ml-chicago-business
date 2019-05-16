feb72ee08738043008aba7a3cc7a7bf0986e2372
erhla <33432469+erhla@users.noreply.github.com>
2019-05-16 00:51:28 -0500

    working

--- import_data.py
+++ import_data.py
@@ -2,37 +2,19 @@ import requests
 import pandas as pd
 from functools import reduce
 
-#getting chicago open data
-#business xqx5-8hwx
-#blocks bt9m-d2mf
-
-'''
-os = 0
-params = {'$limit': 1000, '$offset': os}
-response = requests.get('https://data.cityofchicago.org/resource/xqx5-8hwx.json', params).json()
-total_response = response
-os = 1000
-while len(response) >= 1000:
-    params = {'$limit': 1000, '$offset': os}
-    response = requests.get('https://data.cityofchicago.org/resource/xqx5-8hwx.json', params).json()
-    os += 1000
-    total_response += response
-
-business_df = pd.DataFrame.from_dict(total_response)
-business_df.columns
-
-to_drop_lst = []
-for col in business_df.columns:
-  if "computed" in col:
-    to_drop_lst.append(col)
-business_df.drop(to_drop_lst, axis=1, inplace=True )
-'''
+def get_chicago_data():
+    CSV_URL = 'https://data.cityofchicago.org/api/views/'
+    CSV_FRAG = '/rows.csv?accessType=DOWNLOAD'
+    URLS = {'business': CSV_URL + 'xqx5-8hwx' + CSV_FRAG,
+            'blocks': CSV_URL + 'bt9m-d2mf' + CSV_FRAG,
+            '311': CSV_URL + 'v6vf-nfxy' + CSV_FRAG}
+    for key, val in URLS.items():
+        df = pd.read_csv(val)
+        print(key, 'download complete')
+        df.to_csv(key + '.csv')
+        print(key, 'saved')
 #getting ACS data
-
-acs_variables = pd.read_csv('acs_variables.csv')
-ACS_API = "https://api.census.gov/data/2017/acs/acs5"
-
-def get_acs_data(df):
+def get_acs_data(df, ACS_API):
     parameters = {
         "get": ",".join(var for var in df.Variable.to_list()),
         "for": "block group:*",
@@ -42,14 +24,20 @@ def get_acs_data(df):
     cols = data.pop(0)[-4:]
     return pd.DataFrame(data, columns=df.Description.to_list() + cols)
 
-def compile_census_data(acs_variables):
+def compile_census_data(acs_variables, year):
     LOCATION_VARIABLES = ["state", "county", "tract", "block group"]
+    ACS_API = "https://api.census.gov/data/" + str(year) + "/acs/acs5"
     data = []
-    for i in range(acs_variables.Dataset.max()):
+    for i in range(acs_variables['Dataset'].max()):
         current = acs_variables[acs_variables['Dataset'] == i+1]
-        data.append(get_acs_data(current))
+        data.append(get_acs_data(current, ACS_API))
     merged = reduce(lambda left, right: pd.merge(left, right, how='outer', on=LOCATION_VARIABLES), data)
     merged["block_group"] = merged.apply(lambda row: "".join(str(row[var]) for var in LOCATION_VARIABLES), axis=1)
     return merged.drop(columns=LOCATION_VARIABLES)
-        
-census_data = compile_census_data(ACS_VARIABLES)
+
+def save_census_data():
+    acs_variables = pd.read_csv('acs_variables.csv')
+    acs_17 = compile_census_data(acs_variables, 2017)
+    acs_13 = compile_census_data(acs_variables, 2013)
+    acs_17.to_csv('acs_17.csv')
+    acs_13.to_csv('acs_13.csv')
---
+++ process_data.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 16 00:44:44 2019
+
+@author: erhla
+"""
+
