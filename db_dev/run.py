7ec6de16e21bb81a5f97a6cc43765f1e6e2614a5
Patrick Lavallee <pld@cs.uchicago.edu>
2019-05-23 17:13:25 -0500

    Finished working draft of db.

--- db_dev/run.py
+++ db_dev/run.py
@@ -19,13 +19,12 @@ def run_chicago_storefronts_database(arguments):
 
     db = csdb.Storefronts()
     db.open()
-    if arguments.create_licenses_table:
-        db.create_licences_table()
-    if arguments.populate_licenses_table:
-        db.populate_licenses_table()
-    if arguments.create_storefronts_table:
+    if arguments.no_download:
         db.create_storefronts_table()
-    if arguments.populate_storefronts_table:
+        db.populate_storefronts_table()
+    else:
+        db.create_licences_table()
+        db.populate_licenses_table()
         db.create_storefronts_table()
         db.populate_storefronts_table()
     db.close()
@@ -37,36 +36,12 @@ if __name__ == "__main__":
         description="Create the Chicago Storefronts database."
     )
     parser.add_argument(
-        "--cl",
+        "--no_download",
         action="store_const",
         const=True,
         default=False,
-        help="Create the licenses table.",
-        dest="create_licenses_table"
-    )
-    parser.add_argument(
-        "--pl",
-        action="store_const",
-        const=True,
-        default=False,
-        help="Populate the licenses table.",
-        dest="populate_licenses_table"
-    )
-    parser.add_argument(
-        "--cs",
-        action="store_const",
-        const=True,
-        default=False,
-        help="Create the storefronts table.",
-        dest="create_storefronts_table"
-    )
-    parser.add_argument(
-        "--ps",
-        action="store_const",
-        const=True,
-        default=False,
-        help="Populate the storefronts table.",
-        dest="populate_storefronts_table"
+        help="Create database without rerequestingfrom Chicago Data Portal API.",
+        dest="no_download"
     )
     arguments = parser.parse_args()
     run_chicago_storefronts_database(arguments)
