c014450757f9aa0fa5efb0f57b1215881c1f7307
Patrick Lavallee <pld@cs.uchicago.edu>
2019-06-05 14:07:01 -0500

    Added first run of pipeline with one temporal split.

--- db_dev/run_pb.py
+++ db_dev/run_pb.py
@@ -29,8 +29,13 @@ def run_entrepreneurship_pipeline(arguments):
     vl_bound = np.datetime64(arguments.vl_bound)
     vu_bound = np.datetime64(arguments.vu_bound)
     ce.temporal_splits = (tl_bound, tu_bound, vl_bound, vu_bound)
-    # Model on this temporal split.
-    ce.classify()
+    # Model on this temporal split otherwise.
+    if not arguments.skip_model:
+        ce.classify()
+    # Just create the datasets for the temporal split otherwise.
+    else:
+        ce._db_request(tl_bound, tu_bound, vl_bound, vu_bound, True)
+        ce._db_request(tl_bound, tu_bound, vl_bound, vu_bound, False)
 
 
 if __name__ == "__main__":
@@ -57,6 +62,14 @@ if __name__ == "__main__":
         "--vu_bound",
         help="Temporal upper bound exclusive for validation set.",
         dest="vu_bound"
+    )
+    parser.add_argument(
+        "--skip_model",
+        action="store_const",
+        const=True,
+        default=False,
+        help="Skip modeling and only return temporal sets.",
+        dest="skip_model"
     )
     arguments = parser.parse_args()
     run_entrepreneurship_pipeline(arguments)
