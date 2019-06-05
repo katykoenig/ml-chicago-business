b93c01eb346c83b53040ede2312d9e2fe4e6644c
Eric Langowski <langowski@cs.uchicago.edu>
2019-06-05 16:41:35 -0500

    patched

--- import_data.py
+++ import_data.py
@@ -35,16 +35,15 @@ def get_acs_data(df, ACS_API):
     '''
     '''
     parameters = {
-        "get": ",".join(var for var in df.Variable.to_list()),
+        "get": ",".join(var for var in list(df.Variable)),
         "for": "block group:*",
         "in": "state:17 county:031"} # Cook County, Illinois
-    try:
-        request = requests.get(ACS_API, params=parameters)
-        data = request.json()
-        cols = data.pop(0)[-4:]
-        return pd.DataFrame(data, columns=df.Description.to_list() + cols)
-    except:
-        return None
+
+    request = requests.get(ACS_API, params=parameters)
+    data = request.json()
+    cols = data.pop(0)[-4:]
+    return pd.DataFrame(data, columns=list(df.Description) + cols)
+
 
 
 def compile_census_data(acs_variables, year):
@@ -55,10 +54,8 @@ def compile_census_data(acs_variables, year):
     data = []
     for i in range(acs_variables['Dataset'].max()): #loop over ACS tables
         current = acs_variables[acs_variables['Dataset'] == i+1]
-        print(current)
         result = get_acs_data(current, ACS_API)
-        if result:
-            data.append(result)
+        data.append(result)
     merged = reduce(lambda left, right: pd.merge(left, right, how='outer', on=LOCATION_VARIABLES), data)
     merged["block_group"] = merged.apply(lambda row: "".join(str(row[var]) for var in LOCATION_VARIABLES), axis=1)
     return merged.drop(columns=LOCATION_VARIABLES)
@@ -67,7 +64,7 @@ def compile_census_data(acs_variables, year):
 def save_census_data():
     '''
     '''
-    acs_variables = pd.read_csv('acs_variables.csv')
+    c
     acs_17 = compile_census_data(acs_variables, 2017)
     acs_13 = compile_census_data(acs_variables, 2013)
     acs_17.to_csv('acs_17.csv')
