34568447341a1fc8021842336cc9dacfe116af9d
Patrick Lavallee <pld@cs.uchicago.edu>
2019-06-12 10:24:15 -0500

    Draft aequitas analysis.

--- aq_analyze.py
+++ aq_analyze.py
@@ -28,18 +28,28 @@ METRICS = [
 def aq_analyze(arguments):
 
     # Import valid set and block scores from best model.
-    block_scores = pd.read_csv(arguments.block_scores) \
-        .rename(columns={0: "block_group", 1: "score"})
+    block_scores = pd.read_csv(
+        arguments.block_scores, header=None, names=["block_group", "score"]
+    )
     valid_set = pd.read_csv(arguments.valid_set) \
         .drop_duplicates(subset="block_group") \
         .merge(block_scores, on="block_group") \
-        .rename(columns={"success": "label_value"})
-    features_to_drop = ["block", "block_group"] + [
+        .rename(columns={"successful": "label_value"})
+    # Drop columns not needed for analysis.
+    features_to_drop = [
+        "block", "block_group", "sf_id", "earliest_issue", "latest_issue"
+    ]
+    features_to_drop += [
         column
         for column in valid_set.columns
         if ("crime" in column or "license" in column) and "count" not in column
     ]
     valid_set = valid_set.drop(columns=features_to_drop)
+    # Initialize base comparison attributes dictionary.
+    base_comparison = {
+        "pct_white": None,
+        "pct_high_income": None
+    }
     # Preprocess outside of Aequitas because preprocess_input_df() doesn't work.
     for column in valid_set.columns:
         if column == "score":
@@ -48,12 +58,18 @@ def aq_analyze(arguments):
             valid_set[column] = valid_set[column].astype(int)
         else:
             if valid_set[column].nunique() > 1:
-                valid_set[column] = pd.qcut(
+                valid_set[column], bins = pd.qcut(
                     x=valid_set[column],
                     q=4,
                     precision=2,
-                    duplicates="drop"
+                    duplicates="drop",
+                    retbins=True
                 )
+                # Save label of highest quartile for base comparison attributes.
+                if column in base_comparison:
+                    lb = str(round(bins[3], 2))
+                    ub = str(round(bins[4], 2))
+                    base_comparison[column] = "(" + lb + ", " + ub + "]"
             valid_set[column] = valid_set[column].astype(str)
     # Initialize Aequitas objects and export directory.
     aqg, aqb, aqf, aqp = Group(), Bias(), Fairness(), Plot()
@@ -70,34 +86,23 @@ def aq_analyze(arguments):
         .round(2) \
         .to_csv(directory + "/aequitas_crosstabs.csv", index=False)
     # Calculate and plot bias with respect to white, high income communities.
-    base_comparison = {
-        "pct_white": "(0.67, 1.0]",
-        "pct_high_income": "(0.25, 0.73]"
-    }
     disparity_white_hiinc = aqb.get_disparity_predefined_groups(
         crosstabs.loc[crosstabs["attribute_name"].isin(base_comparison.keys())],
         valid_set, base_comparison
     )
-    a = aqp.plot_disparity_all(disparity_white_hiinc, metrics=METRICS)
-    a.save_fig(directory + "/bias_white_high_income.png")
+    a = aqp.plot_disparity_all(
+        disparity_white_hiinc,
+        metrics=METRICS,
+        show_figure=False
+    )
+    a.savefig(directory + "/bias_ref_white_high_income.png")
     # Calculate and plot fairness with respect to the same.
     b = aqp.plot_fairness_disparity_all(
         aqf.get_group_value_fairness(disparity_white_hiinc),
-        metrics=METRICS
+        metrics=METRICS,
+        show_figure=False
     )
-    b.save_fig(directory + "/fairness_white_high_income.png")
-    # Calculate and plot bias with respect to majority group.
-    disparity_major_group = aqb.get_disparity_major_group(
-        crosstabs, valid_set
-    )
-    c = aqp.plot_disparity_all(disparity_major_group, metrics=METRICS)
-    c.save_fig(directory + "/bias_majority_group.png")
-    # Calculate and plot fairness with respect to the same.
-    d = aqp.plot_fairness_disparity_all(
-        aqf.get_group_value_fairness(disparity_major_group),
-        metrics=METRICS
-    )
-    d.save_fig(directory + "/fairness_majority_group.png")
+    b.savefig(directory + "/fairness_ref_white_high_income.png")
 
 
 if __name__ == "__main__":
